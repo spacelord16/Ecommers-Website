5c580f5e9ae43a389386d0b4813dafc9b4f39aa8
Aditya <deshpande.aditya16@gmail.com>
2020-04-23 15:40:05 +0530

    Update..

--- myc/myc/settings.py
+++ myc/myc/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Managing Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
